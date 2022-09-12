b87332f37e0a0ad42337a997a71789bf1bed9b7d
Le1ec0 <leandroizidoro@hotmail.com>
2022-09-12 14:30:18 -0300

    inserted elements

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
+import { Card } from "react-native-elements";
+import styles from "./AppStyle.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card>
+        <Card.Image source={require("./assets/beef-burguer.png")} />
+        <Card.Title style={styles.itemInfo}>Beef Burguer</Card.Title>
+        <Text style={styles.caloriesInfo}>🔥70 Calories</Text>
+        <Text style={styles.priceInfo}>$12.00</Text>
+      </Card>
+      <Card>
+        <Card.Image source={require("./assets/pancakes.png")} />
+        <Card.Title style={styles.itemInfo}>Pancakes</Card.Title>
+        <Text style={styles.caloriesInfo}>🔥60 Calories</Text>
+        <Text style={styles.priceInfo}>$15.00</Text>
+      </Card>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ AppStyle.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemInfo: {
+    fontSize: 30,
+    color: "black",
+    marginBottom: 10,
+  },
+  caloriesInfo: {
+    fontSize: 20,
+    color: "black",
+    marginBottom: 10,
+  },
+  priceInfo: {
+    fontSize: 20,
+    color: "yellow",
+    marginBottom: 10,
+  },
+});
+
+export default styles;
